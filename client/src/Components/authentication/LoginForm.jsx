b2486df11d53028ee1cf3d7cff82b0c8a2310684
Ethan Brown <eb934053@gmail.com>
2025-12-02 16:40:39 -0600

    Removed unused Navigate component

--- client/src/Components/authentication/LoginForm.jsx
+++ client/src/Components/authentication/LoginForm.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { useAuth } from "../../context/context"
-import { Navigate, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 export const LoginForm = () => {
     const navigate = useNavigate()
